18f4e88d4daa59dd9b5a8b2ec1c3f3449ed08a0f
savekartik-collab <savekartik@gmail.com>
2025-12-26 18:26:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,17 @@
-# Rexbots
-# Don't Remove Credit
-# Telegram Channel @RexBots_Official
-
-# Rexbots
-# Don't Remove Credit 
-# Telegram Channel @RexBots_Official
 import os
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
+
+API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
+
+ADMINS = [int(i) for i in os.environ.get("ADMINS", "").split(",") if i]
+
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
-LOG_CHANNEL = 
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+
+ERROR_MESSAGE = os.environ.get("ERROR_MESSAGE", "True").lower() == "true"
+
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
-# Rexbots
-# Don't Remove Credit
-# Telegram Channel @RexBots_Official
-
-
-# Rexbots
-# Don't Remove Credit 
-# Telegram Channel @RexBots_Official
-
-# Rexbots
-# Don't Remove Credit
-# Telegram Channel @RexBots_Official
